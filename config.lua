1861e780f571f1a4c24ec72790cf4c5e73ef0841
Ryan Lockard <ryan.jesse.lockard@gmail.com>
2019-11-10 18:53:05 -0500

    added configurable emote, tweet, and chat colors

--- config.lua
+++ config.lua
@@ -3,3 +3,7 @@ Config.Locale = 'en'
 
 Config.EnableESXIdentity = false -- only turn this on if you are using esx_identity and want to use RP names
 Config.OnlyFirstname     = false
+
+Config.ColorEmote  = { 159, 89, 217 }
+Config.ColorTwt    = { 56, 161, 243 }
+Config.ColorChat   = { 227, 227, 227 }
